4a78e018cbe31e7242cd8e6a8d5ff3bc6e7f6de9
Saliha713 <salihahaider713@gmail.com>
2024-09-22 19:27:26 +0500

    resolving path issues

--- profile.js
+++ profile.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-// JavaScript object with all the provided data
+
 const profileData = {
     personalInfo: {
         name: "Saliha Haider",
@@ -41,46 +40,3 @@ const profileData = {
 };
 
  
-=======
-// JavaScript object with all the provided data
-const profileData = {
-    personalInfo: {
-        name: "Saliha Haider",
-        age: 20,
-        email: "salihahaider713@gmail.com",
-        phone: "03338923054",
-        introduction: "My name is Saliha Haider, a 20-year-old passionate learner with a drive for continuous growth and development. I am currently focused on honing my skills in data, Web development, and various programming languages, while also pursuing personal interests in calligraphy, sketching, and reading books. I have a strong aptitude for mathematics and enjoy teaching the subject.",
-        image:{src:"id.jpg", description:"My pic"},
-        quote: "ٱهْدِنَا ٱلصِّرَٰطَ ٱلْمُسْتَقِيمَ",
-        translation: "(O Allah!) Guide us to the straight path."
-    },
-    skills: {
-        softSkills: ["Team Work", "Communication", "Problem-solving"],
-        hardSkills: ["Database Management", "Data Analysis", "C++, Python, JavaScript"]
-    },
-    projects: [
-        "Chess game in C++",
-        "Snake game in C++",
-        "Paint app in python",
-        "Arranged a bake sale"
-    ],
-    awardsAndCertifications: [
-        "Awarded for A+ grade in matric, Queen Mary College, Lahore",
-        "Awarded merit-based scholarship, Punjab College, Lahore",
-        "Awarded merit-based scholarship for two semesters, IT university, Lahore"
-    ],
-    education: [
-        "Matric in Computer Science from Queen Mary College, Lahore (Completed: 2020)",
-        "Intermediate in Computer Science from Punjab College, Lahore (Completed: 2022)",
-        "Doing Bachelors in Computer Science from IT university, Lahore"
-    ],
-    videos: [
-        { src: "snake.mp4", description: "Snake Game" },
-        { src: "paint.mp4", description: "Paint App" }
-    ],
-    socialMedia: {
-        linkedIn: "https://www.linkedin.com/in/saliha-haider-a34387267?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-    }
-};
-
->>>>>>> 102589261c2b2e34949b22ce636e2a9a54d411fd
